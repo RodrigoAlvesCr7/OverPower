bab791ce4cc7927ae6062ea013cbca60a5f869a5
ICFallenWarrior <96588503+ICFallenWarrior@users.noreply.github.com>
2023-11-21 11:33:51 +0000

    Add files via upload

---
+++ Assets/Scenes/TankStats.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+//[CreateAssetMenu(fileName = "TankData", menuName = "OverPower/TankData" )]
+public class TankStats : MonoBehaviour
+{
+    public string tankName;
+
+    public int speedPrio;
+
+    public int fuelCap;
+
+    public int maxHealth;
+
+    public int currentHealth;
+}
