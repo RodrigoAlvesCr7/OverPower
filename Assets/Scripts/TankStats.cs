f0b20e531bfa191c949d96e6cc5ad1aa697d4037
ICFallenWarrior <pedromourinha.playz@gmail.com>
2023-11-22 11:28:55 +0000

    Fixed up file names and locations

--- Assets/Scripts/TankStats.cs
+++ Assets/Scripts/TankStats.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 
-//[CreateAssetMenu(fileName = "TankData", menuName = "OverPower/TankData" )]
-public class TankStats : MonoBehaviour
+
+[System.Serializable]
+public class TankStats
 {
     public string tankName;
 
